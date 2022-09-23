e23a2f4064cfab09c6a4899d2e114e224930f94f
Emil Örjes <emil.orjes@sogeti.com>
2022-09-23 10:47:00 +0200

    Add Application Service for Account

---
+++ MicroServiceRabbitMQ/MicroServiceRabbitMQ.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using MicroServiceRabbitMQ.Banking_Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServiceRabbitMQ.Banking.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
---
+++ MicroServiceRabbitMQ/MicroServiceRabbitMQ.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using MicroServiceRabbitMQ.Banking.Application.Interfaces;
+using MicroServiceRabbitMQ.Banking_Domain.Interfaces;
+using MicroServiceRabbitMQ.Banking_Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServiceRabbitMQ.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+           return _accountRepository.GetAccounts();
+        }
+    }
+}
