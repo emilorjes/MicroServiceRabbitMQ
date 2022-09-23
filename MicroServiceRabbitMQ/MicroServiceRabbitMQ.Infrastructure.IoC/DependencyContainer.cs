bdc51d84de6cd1d1634f22d05dc1de258a4913e3
Emil Örjes <emil.orjes@sogeti.com>
2022-09-23 07:31:23 +0200

    Add Deoendency service for IEventBus / RabbitMQBus

---
+++ MicroServiceRabbitMQ/MicroServiceRabbitMQ.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroServiceRabbitMQ.Domain.Core.Bus;
+using MicroServiceRabbitMQ.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServiceRabbitMQ.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
