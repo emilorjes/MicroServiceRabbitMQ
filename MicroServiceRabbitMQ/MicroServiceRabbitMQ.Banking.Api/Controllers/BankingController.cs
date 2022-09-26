ef828cd72fdda877894b0cd23da6aa626e5ecdad
Emil Örjes <emil.orjes@sogeti.com>
2022-09-26 08:16:38 +0200

    Move BankingController to controller folder and delete WF controller

---
+++ MicroServiceRabbitMQ/MicroServiceRabbitMQ.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,24 @@
+using MicroServiceRabbitMQ.Banking.Application.Interfaces;
+using MicroServiceRabbitMQ.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroServiceRabbitMQ.Banking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
