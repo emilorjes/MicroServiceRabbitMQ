476377138203dc1cb1aa48b129edbea6c1bacb9b
Emil Örjes <emil.orjes@sogeti.com>
2022-09-27 08:26:57 +0200

    Implement TransferController

---
+++ MicroServiceRabbitMQ/MicroServiceRabbitMQ.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,23 @@
+﻿using MicroServiceRabbitMQ.Transfer.Application.Interface;
+using MicroServiceRabbitMQ.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroServiceRabbitMQ.Transfer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private readonly ITransferService _transferService;
+        public TransferController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TransferLog>> Get()
+        {
+            return Ok(_transferService.GetTranferLogs());
+        }
+    }
+}
