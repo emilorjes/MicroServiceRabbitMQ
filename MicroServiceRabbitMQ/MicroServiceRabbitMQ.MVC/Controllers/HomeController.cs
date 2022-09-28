11ce5c29c92f9896805b58b4e890bf4ffac5307d
Emil Örjes <emil.orjes@sogeti.com>
2022-09-28 09:32:53 +0200

    Add input form in indexview, add Transfer method in transfer service, add Http Client service in Startup, add Transfer Post method in Homecontroller

--- MicroServiceRabbitMQ/MicroServiceRabbitMQ.MVC/Controllers/HomeController.cs
+++ MicroServiceRabbitMQ/MicroServiceRabbitMQ.MVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using MicroServiceRabbitMQ.MVC.Models;
+using MicroServiceRabbitMQ.MVC.Models.DTOs;
+using MicroServiceRabbitMQ.MVC.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace MicroServiceRabbitMQ.MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITransferService _transferService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITransferService transferService)
         {
             _logger = logger;
+            _transferService = transferService;
         }
 
         public IActionResult Index()
@@ -27,6 +31,20 @@ namespace MicroServiceRabbitMQ.MVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel transferModel)
+        {
+            TransferDTO transferDTO = new()
+            {
+                AccountSource = transferModel.SourceAccount,
+                AccountDestination = transferModel.DestinationAccount,
+                TransferAmount = transferModel.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDTO);
+            return View("Index");
         }
     }
 }
--- MicroServiceRabbitMQ/MicroServiceRabbitMQ.MVC/Services/TransferService.cs
+++ MicroServiceRabbitMQ/MicroServiceRabbitMQ.MVC/Services/TransferService.cs
@@ -1,12 +1,25 @@
 ﻿using MicroServiceRabbitMQ.MVC.Models.DTOs;
+using Newtonsoft.Json;
 
 namespace MicroServiceRabbitMQ.MVC.Services
 {
     public class TransferService : ITransferService
     {
-        public Task Transfer(TransferDTO transferDTO)
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
         {
-            throw new NotImplementedException();
+            _apiClient = apiClient;
+        }
+        public async Task Transfer(TransferDTO transferDTO)
+        {
+            string uri = "https://localhost:7051/api/Banking";
+            StringContent transferContent = new(JsonConvert.SerializeObject(transferDTO),
+                System.Text.Encoding.UTF8,
+                "application/json");
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
         }
     }
 }
